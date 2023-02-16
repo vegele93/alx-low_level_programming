b68725003dda41d0a9b88792c93d97dce1599bd9
Victor Egele <vegele93@gmail.com>
2023-02-16 09:50:17 -0800

    program that prints an exact line to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints an exact line to the standard error
+ * Return: Should return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
