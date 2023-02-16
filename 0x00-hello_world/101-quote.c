0be9d49e46f6d5fe5a5a89e858b688183b3502a0
Victor Egele <vegele93@gmail.com>
2023-02-16 10:10:57 -0800

    a program that prints a line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints an exact line to the standard error
+ * main - A program that prints a line to the standard error
  * Return 1 (Success)
  */
 int main(void)
