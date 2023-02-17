84a5c895bdc6523cd7686bbc826b6f65e0eafb4c
Victor Egele <vegele93@gmail.com>
2023-02-17 08:52:35 -0800

    prints alphabets in lower cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower cases
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar (lc);
+	}
+	putchar('\n');
+	return (0);
+}
